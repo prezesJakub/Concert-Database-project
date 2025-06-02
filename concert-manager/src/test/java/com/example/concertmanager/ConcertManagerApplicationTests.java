355647d04a23aa8646e95f18bcd54ff2b197ed36
Jakub Zając <jakubtomaszkrzysztof@gmail.com>
2025-06-02 10:48:31 +0200

    Initialized Spring Boot project

---
+++ concert-manager/src/test/java/com/example/concertmanager/ConcertManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concertmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
